7eed54e20511370a051cbb0b00d796072e42b8e4
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-07-08 09:25:56 +0530

    update

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./login.scss";
 const Login = () => {
+  const LOGIN_URI =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:8888/login"
+      : "https://boiling-lowlands-64949.herokuapp.com/login";
+
   return (
     <div>
       <div className="login_container">
         <h1>Spotify Profile</h1>
-        <a href="http://localhost:8888/login">
+        <a href={LOGIN_URI}>
           <button size="lg" variant="success">
             login with Spotify
           </button>
